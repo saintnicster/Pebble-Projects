7a3a9df70739cff1ae4583a7c564f29e9a5571df
Nick Fajardo <saintnicster@gmail.com>
2013-04-19 19:28:08 -0500

    Initial code dump

--- counter/src/counter.c
+++ counter/src/counter.c
@@ -1,7 +1,10 @@
 #include "pebble_os.h"
 #include "pebble_app.h"
 #include "pebble_fonts.h"
-
+#define BUFFER_SIZE 50
+#define ONES_SELECTED 1
+#define TENS_SELECTED 10
+#define HUNDRED_SELECTED 100
 
 #define MY_UUID { 0xE9, 0xFA, 0x71, 0xA1, 0xD0, 0x27, 0x43, 0xB7, 0xAB, 0xE6, 0xFC, 0xE1, 0xB7, 0x1F, 0xCC, 0xC8 }
 PBL_APP_INFO(MY_UUID,
@@ -11,6 +14,33 @@ PBL_APP_INFO(MY_UUID,
              APP_INFO_STANDARD_APP);
 
 Window window;
+
+struct CommonWordsData {
+  bool is_negative;
+  short counter;
+  int selected_digit;
+} s_data;
+
+void move_pointer() {
+  switch (s_data.selected_digit) {
+    case ONES_SELECTED:
+      s_data.selected_digit = TENS_SELECTED;
+      break;
+    case TENS_SELECTED:
+      s_data.selected_digit = HUNDRED_SELECTED;
+      break;
+    case HUNDRED_SELECTED:
+      s_data.selected_digit = ONES_SELECTED;
+      break;
+  }
+}
+
+void decrement_counter(short how_much) {
+  s_data.counter -= how_much;
+}
+void increment_counter(short how_much) {
+  s_data.counter += how_much;
+}
 
 
 void handle_init(AppContextRef ctx) {
