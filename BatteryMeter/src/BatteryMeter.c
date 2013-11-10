1fb9353054357ce125804c7bf85892550bb851ad
Nick Fajardo <saintnicster@gmail.com>
2013-11-10 13:28:13 -0600

    Initial prototype work

--- BatteryMeter/src/BatteryMeter.c
+++ BatteryMeter/src/BatteryMeter.c
@@ -2,48 +2,54 @@
 
 static Window *window;
 static TextLayer *text_layer;
-
-static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Select");
+static Layer *battery_layer;
+void draw_battery_layer(Layer *layer, GContext *ctx) {
+  GRect bounds = layer_get_bounds( layer );
+  
+  graphics_context_set_stroke_color( ctx, GColorBlack );
+  graphics_draw_rect               ( ctx, bounds );
+  //graphics_fill_rect               ( ctx, bounds, 0, GCornerNone );
 }
-
-static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Up");
-}
-
-static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Down");
-}
-
-static void click_config_provider(void *context) {
-  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
-  window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
-  window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
-}
-
 static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
-  text_layer_set_text(text_layer, "Press a button");
-  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
-  layer_add_child(window_layer, text_layer_get_layer(text_layer));
+  text_layer = text_layer_create( GRect(10, 10, bounds.size.w - 20, 50) );
+  text_layer_set_text_alignment ( text_layer,   GTextAlignmentCenter );
+  text_layer_set_font           ( text_layer,   fonts_get_system_font( FONT_KEY_GOTHIC_28 ) );
+  text_layer_set_text           ( text_layer,   "Level - 00%" );
+  text_layer_set_text_color     ( text_layer,   GColorBlack);
+  layer_add_child               ( window_layer, text_layer_get_layer(text_layer) );
+
+  battery_layer = layer_create(GRect(57, 60, 30, 80 ));
+  layer_set_update_proc       (battery_layer, (LayerUpdateProc) draw_battery_layer);
+  layer_add_child( window_layer, battery_layer );
 }
 
 static void window_unload(Window *window) {
-  text_layer_destroy(text_layer);
+  text_layer_destroy( text_layer );
+  layer_destroy     ( battery_layer );
 }
-
+void handle_battery(BatteryChargeState charge_state) {
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Handling battery state... charge_percent = %d%%, is_charging = %i, is_plugged = %i", charge_state.charge_percent, charge_state.is_charging, charge_state.is_plugged);
+  static char level_text[] = "Level - 100%";
+  
+  snprintf( level_text, sizeof(level_text), "Level - %d%%", charge_state.charge_percent);
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "%s", level_text);
+  text_layer_set_text( text_layer,  level_text );
+}
 static void init(void) {
   window = window_create();
-  window_set_click_config_provider(window, click_config_provider);
   window_set_window_handlers(window, (WindowHandlers) {
     .load = window_load,
     .unload = window_unload,
   });
   const bool animated = true;
   window_stack_push(window, animated);
+
+  battery_state_service_subscribe( (BatteryStateHandler)handle_battery );
+
+  handle_battery(battery_state_service_peek());
 }
 
 static void deinit(void) {
@@ -53,8 +59,7 @@ static void deinit(void) {
 int main(void) {
   init();
 
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", window);
-
   app_event_loop();
+  
   deinit();
 }
